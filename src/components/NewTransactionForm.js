30556a1d0b9624b4f9794756df6a5794892484d4
ndalilavin <vinson.steve@student.moringaschool.com>
2022-07-29 11:57:52 +0300

    Added form submit functionality on NewTransactionForm

--- src/components/NewTransactionForm.js
+++ src/components/NewTransactionForm.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { uniqueId } from '../utils';
 
 function NewTransactionForm() {
+    const [nameValue, setNameValue] = useState('');
+    const [amountValue, setAmountValue] = useState('');
+
+    const addTransaction = type => {  
+        const data = { id: uniqueId(), name:nameValue, amount: amountValue, type: type };
+
+        console.log(data);
+    }
+
     return (
         <div>
             <h2>Add New Transaction</h2>
@@ -8,18 +18,20 @@ function NewTransactionForm() {
                 <label>
                     Name
                     <div>
-                        <input type="text" />
+                        <input type="text" value={nameValue}
+                            onChange={(e) => setNameValue(e.target.value)} />
                     </div>
                 </label>
                 <label>
                     Amount
                     <div>
-                        <input type="number" />
+                        <input type="number" value={amountValue} 
+                            onChange={(e) => setAmountValue(e.target.value)} />
                     </div>
                 </label>
                 <div>
-                    <button>Income</button>
-                    <button>Expense</button>
+                    <button onClick={() => addTransaction('income')}>Income</button>
+                    <button onClick={() => addTransaction('expense')}>Expense</button>
                 </div>
             </form>
         </div>
