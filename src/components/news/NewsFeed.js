a73624e6eb82294c05f1cbba70099bae4bf1a4ab
ndalilavin <vinson.steve@student.moringaschool.com>
2022-07-31 17:40:57 +0300

    Added NewsObect function with props

--- src/components/news/NewsFeed.js
+++ src/components/news/NewsFeed.js
@@ -1,11 +1,19 @@
- import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 
  const NewsFeed = () => {
-    const [newsItem, setNewsArticle] = useState([])
+    const [articles, setArticles] = useState([])
     
     useEffect(() => {
-        
-    })
+        const getArticles = async () => {
+            const response = await axios.get('https://newsapi.org/v2/everything?q=shopping&apiKey=07764aca70b44596ba2e4886983a4313')
+            console.log(response)
+            setArticles(response.data.articles)
+        }
+        getArticles()
+    },[])
+
     return (
         <div>
             
---
+++ src/components/news/NewsObject.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './NewsObject.css';
+
+const NewsObject = ({ title, description, url, urlToImage}) => {
+    return (
+        <div className="news-app">
+            <div className="news-object">
+                <img className="news-img" src={urlToImage} alt={urlToImage}/>
+                <h3><a href={url}>{title}</a></h3>
+                <p>{description}</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default NewsObject;
